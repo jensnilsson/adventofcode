31d0ebaddcdc40e6cee77f2a33118ff78c3a5c9f
alexandraone <onegard@kth.se>
2024-12-05 08:56:33 +0100

    Day3 part1

---
+++ 2024/day3/js/part2.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+fs.readFile("inputAlex.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  parseFile(data);
+});
+
+function parseFile(fileData) {
+  let answer = 0;
+
+  const regex = /mul\(\d+,\d+\)/g;
+  const matches = fileData.match(regex);
+
+  matches.map((match) => {
+    const values = match
+      .replace("mul(", "")
+      .replace(")", "")
+      .split(",")
+      .map(Number);
+
+    answer += values[0] * values[1];
+  });
+
+  console.log("answer", answer);
+}
