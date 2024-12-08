1693b81503b190344208192e69438fc08afb2cf4
Alexandra Onegård <alexandra.onegard@dynabyte.se>
2024-12-08 20:18:23 +0100

    Day5 part1 js

--- 2024/day5/js/part1.js
+++ 2024/day5/js/part1.js
@@ -1,28 +1,54 @@
-const fs = require("fs");
-fs.readFile("inputJens.txt", "utf8", (err, data) => {
+const fs = require('fs');
+fs.readFile('inputAlex.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  // Jens
+
   parseFile(data);
 });
 
 function parseFile(fileData) {
+  let hasSeenEmptyLine = false;
+  const ruleMap = {};
   let answer = 0;
 
-  const regex = /mul\(\d+,\d+\)/g;
-  const matches = fileData.match(regex);
+  fileData.split(/\r?\n/).forEach((line) => {
+    if (line === '') {
+      hasSeenEmptyLine = true;
+      return;
+    }
+    if (!hasSeenEmptyLine) {
+      const rule = line.split('|');
+      if (!ruleMap[rule[1]]) {
+        ruleMap[rule[1]] = [rule[0]];
+      } else {
+        ruleMap[rule[1]].push(rule[0]);
+      }
+    }
 
-  matches.map((match) => {
-    const values = match
-      .replace("mul(", "")
-      .replace(")", "")
-      .split(",")
-      .map(Number);
+    if (hasSeenEmptyLine) {
+      let isValid = true;
+      const updateList = line.split(',');
+      updateList.forEach((entry, index) => {
+        const remainingEntries = updateList.slice(index + 1);
+        const valuesBefore = ruleMap[entry];
 
-    answer += values[0] * values[1];
+        const filteredArray = remainingEntries.filter((value) =>
+          valuesBefore?.includes(value)
+        );
+
+        if (filteredArray.length > 0) {
+          isValid = false;
+          return;
+        }
+      });
+
+      if (isValid) {
+        const middleIndex = (updateList.length - 1) / 2;
+        answer += parseInt(updateList[middleIndex]);
+      }
+    }
   });
-
-  console.log("answer", answer);
+  console.log(answer);
 }
